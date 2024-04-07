aed1a585121ee39a807568696f6939bb9ef1b6f6
Khanh Du Vu <khanhdu1211@gmail.com>
2024-04-07 10:37:48 +0700

    orm structure

--- src/server.js
+++ src/server.js
@@ -3,12 +3,16 @@ import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import connection from "./config/connectDB";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// test DB connection
+connection();
 
 configViewEngine(app);
 
